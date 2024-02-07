4f3d67fa5335757ecbd5ab488f59255647ebba01
JEsper <jespervnveen@gmail.com>
2024-02-07 18:13:51 +0100

    elke 5 min data naar db

---
+++ data.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import db
+import time
+
+session = requests.Session()
+
+def retrieveData(uri):
+    token = "a83d911c8b57054979190015e2a3f5d823d16f56"
+    url = f"https://garden.inajar.nl{uri}"
+    head = {'Authorization': 'token {}'.format(token)}
+
+    res = session.get(url, headers=head)
+
+    return res.json()
+
+
+def send(devices):
+    print(devices)
+    for x in devices['results']:
+        serial_number = x['serial_number']
+        name = x['name']
+        label = x['label']
+        last_seen = x['last_seen']
+        battery = x['last_battery_voltage']
+
+        db.cur.execute("INSERT INTO device_data (serial_number, name, label, last_seen, last_battery_voltage) VALUES (%s, %s, %s, %s, %s)", (serial_number, name, label, last_seen, battery))
+        db.conn.commit()
+
+while True:
+    devices = retrieveData('/api/devices/?format=json')
+    send(devices)  # Call the function
+    time.sleep(300)  # Wait for 60 seconds
+
---
+++ db.py
@@ -0,0 +1,19 @@
+import sys
+import mariadb
+
+lol = 'lol'
+
+try:
+    conn = mariadb.connect(
+        user="root",
+        password="Wolfie01",
+        host="localhost",
+        database="sensor_data" 
+
+    )
+    
+except mariadb.Error as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
+cur = conn.cursor()
