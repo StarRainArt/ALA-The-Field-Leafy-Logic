c8adc703908bd9cd4886f31fa16fea4ad2dce0af
EdwinVBN <Katuku.edwin@gmail.com>
2024-02-13 11:25:57 +0100

    5 min

--- Dataophalen.py
+++ Dataophalen.py
@@ -1,5 +1,6 @@
 import requests
 import mariadb
+import time
 
 conn = mariadb.connect(
     user="root",
@@ -8,27 +9,7 @@ conn = mariadb.connect(
     database="sensor_data")
 cur = conn.cursor() 
 
-# myToken = 'a83d911c8b57054979190015e2a3f5d823d16f56'
-# myUrl =  'https://garden.inajar.nl/api/devices/?format=json'
-# head = {'Authorization': 'token {}'.format(myToken)}
-
-# r = requests.get(myUrl, headers=head)
-
 session = requests.Session()
-
-# def split_database(text):
-    # with open("database.txt", "r") as f:
-    #     lines = f.readlines()
-
-
-#    info = text.split(" | ")
-#    device = info[0]
-#    time = info[1]
-#    temperature = info[2]
-
-#    print(device + '\n' + time)
-
-# split_database("Device748181 | 4841914941 | 48")
 
 def dataOphalen(uri):
     myToken = 'a83d911c8b57054979190015e2a3f5d823d16f56'
@@ -37,28 +18,29 @@ def dataOphalen(uri):
     r = session.get(myUrl, headers=head)
     return r.json()
 
-# print(dataOphalen("/api/battery_voltage_events/?format=json"))
-devices = dataOphalen("/api/devices/?format=json")
+def dataVersturen(devices):
 
-# print(devices['results'][1])
+    for x in devices['results']:
 
-for x in devices['results']:
+        serial_number = x['serial_number']
+        name = x['name']
+        label = x['label']
+        last_seen = x['last_seen']
+        last_battery_voltage = x['last_battery_voltage']
+        
+        try: 
+            cur.execute("INSERT INTO device_data (serial_number, name, label, last_seen, last_battery_voltage) VALUES (?, ?, ?, ?, ?)", (serial_number, name, label, last_seen, last_battery_voltage))
+            conn.commit()
+        except mariadb.Error as e: 
+            print(f"Error: {e}")
 
-    serial_number = x['serial_number']
-    name = x['name']
-    label = x['label']
-    last_seen = x['last_seen']
-    last_battery_voltage = x['last_battery_voltage']
-    
-    #insert information 
-    try: 
-        cur.execute("INSERT INTO device_data (serial_number, name, label, last_seen, last_battery_voltage) VALUES (?, ?, ?, ?, ?)", (serial_number, name, label, last_seen, last_battery_voltage))
-        conn.commit()
-    except mariadb.Error as e: 
-        print(f"Error: {e}")
+    cur.execute("SELECT serial_number, name FROM device_data")
 
-#retrieving information 
-cur.execute("SELECT serial_number, name FROM device_data") #WHERE first_name=?", (some_name,))
+    for serial_number, name in cur: 
+        print(f" Name: {name}, Serial Number: {serial_number}")
 
-for device, battery_voltage in cur: 
-    print(f"Name: {name}, Serial Number: {serial_number}")
+while True:
+    devices = dataOphalen("/api/devices/?format=json")
+
+    dataVersturen(devices)
+    time.sleep(300)
