6cb450e5dd16c5b674f334beb93a7938cb130c29
PRdWit <143349091+PRdWit@users.noreply.github.com>
2024-02-29 16:11:22 +0100

    Made new page, opened server

--- LeafyLogic/LeafyBrain/views.py
+++ LeafyLogic/LeafyBrain/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def reports(request):
+    return render(request, "reports.html")
