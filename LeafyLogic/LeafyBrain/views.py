9ec75bea31362c3ccc62ca7399a719d6b07101e1
PRdWit <143349091+PRdWit@users.noreply.github.com>
2024-03-13 13:56:21 +0100

    Changed code to fit with the original

--- LeafyLogic/LeafyBrain/views.py
+++ LeafyLogic/LeafyBrain/views.py
@@ -20,15 +20,35 @@ def home(request):
 
 
 
-def dashboard(request):
-    devicedata = DataPoint.objects.all()
-    human_names = [] 
-    for device in devicedata:
-        human_names.append(device.human_name)
+def dashboard(request, device_id):
+    device_data = DeviceData.objects.filter(device_id=device_id)
+    filtered_datapoint = DataPoint.objects.filter(device=device_id)
+    unique_human_names = filtered_datapoint.values_list('human_name', flat=True).distinct()
+
+    latest_values = {}
+
+    name_mapping = {
+        'battery_voltage_events': 'Battery Voltage in %',
+        'soil_electric_conductivity_events': 'Soil Conductivity in dS/m',
+        'soil_relative_permittivity_events': 'Water Content in %',
+        'soil_temperature_events': 'Soil Temp. in °C',
+        'par_events': 'Light Level in μmol/(m²s)',
+        'relative_humidity_events': 'Humidity in %'
+    }
+
+    for name in unique_human_names:
+        latest_data_point = DataPoint.objects.filter(human_name=name).latest('timeStamp')
+        latest_values[name_mapping.get(name, name)] = latest_data_point.value
+
+    context = {
+    'device_data': device_data,
+    "filtered_datapoint": filtered_datapoint,
+    "latest_values": latest_values
+    }
     return render(
         request, 
         "dashboard.html",
-        {"human_name": human_names}              
+        context
     )
 
 
