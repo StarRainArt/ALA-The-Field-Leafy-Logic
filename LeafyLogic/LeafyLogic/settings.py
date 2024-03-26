a4f0a6436e49b2878a09abd338653742296ea86f
JEsper <6001807@mborijnland.nl>
2024-03-26 10:06:18 +0100

    v

--- LeafyLogic/LeafyLogic/settings.py
+++ LeafyLogic/LeafyLogic/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sensor_data',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Wolfie01',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
