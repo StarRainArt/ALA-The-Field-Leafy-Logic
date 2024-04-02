5ebf30009d551c5bb3e2c2f73554530feff35ab9
PRdWit <P.D.business@outlook.com>
2024-04-02 10:24:58 +0200

    Changed back password

--- LeafyLogic/LeafyLogic/settings.py
+++ LeafyLogic/LeafyLogic/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sensor_data',
         'USER': 'root',
-        'PASSWORD': 'edwin',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
